3a88b04f4e0452915bc413af77cd69fe07f41840
Setoue <proshigeru@gmail.com>
2024-06-29 21:22:14 -0300

    refactor: add new dependence and method getAll

--- src/main/java/com/menu_s/menu/MenuApplication.java
+++ src/main/java/com/menu_s/menu/MenuApplication.java
@@ -8,7 +8,7 @@ public class MenuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MenuApplication.class, args);
-		System.out.println("Topa");
+		System.out.println("T123");
 	}
 
 }
--- src/main/java/com/menu_s/menu/controller/FoodController.java
+++ src/main/java/com/menu_s/menu/controller/FoodController.java
@@ -1,5 +1,13 @@
 package com.menu_s.menu.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("food")
 public class FoodController {
     
+    @GetMapping
+    public void getAll(){}
 }

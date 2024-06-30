445ac993802e921d8654d120af07f1cc0d0f1eec
Setoue <proshigeru@gmail.com>
2024-06-30 02:56:16 -0300

    refactor: add method save and fix application.properties

--- src/main/java/com/menu_s/menu/controller/FoodController.java
+++ src/main/java/com/menu_s/menu/controller/FoodController.java
@@ -1,15 +1,15 @@
 package com.menu_s.menu.controller;
 
-import java.util.List;
-
-import com.menu_s.menu.food.FoodResponseDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.menu_s.menu.food.Food;
 import com.menu_s.menu.food.FoodRepository;
+import com.menu_s.menu.food.FoodRequestDTO;
+import com.menu_s.menu.food.FoodResponseDTO;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("food")
@@ -17,7 +17,16 @@ public class FoodController {
 
     @Autowired
     private FoodRepository repository;
-    
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<FoodResponseDTO> getAll(){
         return repository.findAll().stream().map(FoodResponseDTO::new).toList();
---
+++ src/main/java/com/menu_s/menu/food/FoodRequestDTO.java
@@ -0,0 +1,4 @@
+package com.menu_s.menu.food;
+
+public record FoodRequestDTO( String image, Integer price, String title) {
+}

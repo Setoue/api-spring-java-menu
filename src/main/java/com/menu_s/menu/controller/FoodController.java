ea5c988f18445e1d275fd94b843511ea7d137451
Setoue <proshigeru@gmail.com>
2024-06-29 23:42:26 -0300

    refactor: add method get and fix application.properties

--- src/main/java/com/menu_s/menu/controller/FoodController.java
+++ src/main/java/com/menu_s/menu/controller/FoodController.java
@@ -1,13 +1,24 @@
 package com.menu_s.menu.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.menu_s.menu.food.Food;
+import com.menu_s.menu.food.FoodRepository;
+
 @RestController
 @RequestMapping("food")
 public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
     
     @GetMapping
-    public void getAll(){}
+    public List<Food> getAll(){
+        return repository.findAll();
+    }
 }
